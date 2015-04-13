70ed120006b0f6e65b137ccdf17501f370cfacfa
voltronik <voltronik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-13 20:26:15 +0000

    v1.1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/bnfw/tags/1.1.5.1@1133938 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,216 @@
+=== Better Notifications for WordPress ===
+Contributors: voltronik
+Tags: notifications, email, alerts, roles, users, HTML
+Requires at least: 3.5
+Tested up to: 4.1.1
+Stable tag: 1.1.5.1
+License: GPLv2 or later
+
+Send customisable HTML emails to your users for different WordPress notifications.
+
+== Description ==
+
+> Recently updated to be even easier to use!
+
+Better Notifications for WordPress allows you to generate custom HTML email notifications and send them to user roles (including custom roles) or individual users for all kinds of things happening on your WordPress website. Emails are sent out via your WordPress website (using `wp_mail`) but can be sent via SMTP using an appropriate 3rd party plugin should you wish.
+
+= For example: =
+You want a user with the editor role (or all users using the Editor role) to be alerted via email when a new post is published and you'd like to customise it to include your branding along with the author's display name and post time - with this plugin, that's easy. 
+
+Notifications that are currently available to use are: 
+
+**WordPress Defaults**
+
+* New Comment / Comment Awaiting Moderation
+* New Trackback
+* New Pingback
+* Lost Password (For Admin)
+* New User Registration (For Admin)
+
+**Transactional**
+
+* Lost Password (For User)
+* New User Registration (For User)
+* New User - Welcome Email
+
+**Posts / Custom Post Types**
+
+* New Post Published
+* Post Updated
+* Post Pending Review
+* Post Scheduled
+
+**Pages**
+
+* New Page Published
+* Page Updated
+* Page Pending Review
+* Page Scheduled
+* Page - New Comment
+
+**Posts**
+
+* New Category
+* New Tag
+
+**Custom Post Types**
+
+* New Term
+
+If you'd like to see a notification in the list above, please drop me a line in the forums and we'll see what we can do to add it.
+
+> A long and handy list of shortcodes you can use is available [here](http://www.voltronik.co.uk/wordpress-plugins/better-notifications-for-wordpress/ "Shortcodes for use in Better Notifications for WordPress").
+
+
+Having problems? Please let me know via this plugin's forum so we can address them and let it act as a source of information for future reference and other users.
+
+Like this plugin? Please leave it a great review! 
+Don't think the plugin is worthy of 5 stars? Let me know on the forum and we'll do our best to help. 
+
+> Want to add categories and sub-categories via the WordPress front-end? 
+> Try my [Front-end Categories](https://wordpress.org/plugins/front-end-categories/ "Front-end Categories WordPress Plugin") plugin.
+
+
+
+== Installation ==
+
+1. Upload the 'bnfw' plugin folder to the '/wp-content/plugins/' directory or download it via your WordPress installation.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Use the settings in the new 'Notifications' Menu item in the sidebar to configure the plugin.
+4. (Optional) Install a plugin to use SMTP instead of `wp_mail()`. We recommend WP-Mail-SMTP.
+5. Test by creating a notification.
+
+
+
+== Frequently Asked Questions ==
+
+= What are shortcodes? Where can I use them in this plugin? = 
+
+Shortcodes are little blocks of content inserted by WordPress, contained in square brackets. For example, if you want to add the content of a post to a notification for 'New Post Published', you can use the `[post_content]` shortcode to display this in the email that is sent out. 
+
+Shortcodes can be used in the 'Subject' and 'Message Body' of your notifications, except for a select few (due to a restriction in WordPress).
+
+> A long and handy list of shortcodes you can use is available [here](http://www.voltronik.co.uk/wordpress-plugins/better-notifications-for-wordpress/ "Shortcodes for use in Better Notifications for WordPress").
+
+= What are some scenarios this plugin could be used for? =
+
+* Outreach: A blog/news site with hundreds of subscribers and want to use it to alert them of new blog posts. 
+* Communication: A small, internal WordPress site and use it to alert staff of new posts or comments.
+* Monitor: A website for an awesome new product or service and use it for notifications of pingbacks and trackbacks.
+* Security: To receive alerts of password reset requests and their corresponding user.
+
+The possibilities are endless! 
+
+= Does this plugin work with Akismet? =
+
+Yes! There is an option for suppressing comments marked as SPAM by Akismet in the plugin settings.
+
+= What isn't this plugin? =
+
+It's not designed to send out newsletters. There is no send-this-out-on-this-date style functionality included. There are many other great plugins available that you could use for that instead. 
+
+= How do I set-up WordPress to work with this plugin correctly? =
+
+This will very much depend on what notifications you're using the plugin for. Out-of-the-box, this plugin works very well but there are a few tweaks that you will need to ensure 100% compatibility. All the below points refer to this plugin: 
+
+* If you want to use the new comment notifications in BNFW, you need to switch off 'Email me whenever anyone posts a comment' and 'A comment is held for moderation' under Settings > Discussion. It's ok if you don't do this but you might receive WordPress's own email notifications along with the ones you configure using BNFW too. It also goes without saying that you need to enable comments for your posts if you want the email notifications to come through. 
+
+= Configured emails aren't coming through! =
+
+Check your settings to make sure all is as it should be, then check your spam folder and/or filter at your host. Gmail and certain hosts can mark messages from new websites (or IP addresses) as spam so it's worth checking and possibly creating a filter to ensure this doesn't happen in the future.
+
+Many hosts place a limit on the number of emails that can be sent out within an hour so this may also cause some delay in emails arriving. 
+
+= Some of my shortcodes aren't working! =
+
+It's possible you're inserting a shortcode into a notification that cannot use it. For example: the 'New Category' email notification cannot use any of the author or time shortcodes as WordPress only stores the category name, category slug and category description in the database. It's also worth checking the spelling and hyphens in any shortcodes as well as if they are wrapped in square brackets `[]`.
+
+= Can I translate this plugin? =
+
+Yes, of course! The plugin is completely translation-friendly and if you send me your .po file, i'll make sure to include it in the plugin and credit you on this page.
+
+= Will this plugin work with versions WordPress less than 3.5? = 
+
+It might do but this is untested. 
+
+
+
+== Screenshots ==
+
+1. All Notifications
+
+2. Add New Notification
+
+3. Plugin Settings
+
+
+
+== Changelog ==
+
+= 1.1.5.1 =
+* Fix for Custom Post Type notifications not populating shortcodes.
+* Fix for Custom Taxonomy terms not sending out notification emails.
+
+= 1.1.5 =
+* New Shortcode: [permalink].
+* New Notifications: 'Scheduled Posts' and 'Scheduled Pages'.
+* New Shortcodes for the above Notifications: [post_scheduled_date] and [post_scheduled_date_gmt].
+* New Notification: 'New Page Published'.
+* New Notification: 'Page 'Updated'.
+* New Notification: 'Page Pending Review'.
+* New Notification: 'New Page - Comment'.
+* Fixed: A few bugs reported via the forums and some others that i've found - thanks everyone!
+
+= 1.1 =
+* Lots of cool new stuff and more to come soon!
+* Transactional notifications (notifications intended only for the end user) have been added as a new group in the notifications select box.
+* New Notification: Post Pending Review (for all post types).
+* New Notification: New User Registration (Transactional).
+* New Notification: Password Reset (Transactional).
+* New Notification: New User Welcome Email (Transactional).
+* Added an option to enable or disable each notification.
+* Better support for custom taxonomies and terms.
+* Fixed: A few PHP warnings were showing when using `WP_DEBUG`.
+* Fixed: HTML tags being stripped from `[post_content]` and `[comment_content]`.
+* Removed: Some WordPress Multisite shortcodes that didn't work properly. Proper support for Multisite will come later.
+* If you liked this plugin, please leave a review. If you didn't or have a problem, please send me a message in the Support Forum.
+
+= 1.0.2 =
+* Bug fix for `[ID]` shortcode not outputting anything.
+* Bug fix for `[post_category]` showing as empty.
+* Added tags shortcode for use in certain notifications using `[post_tag]`.
+
+= 1.0.1 =
+* Bug fix for notifications not being sent to custom user roles or individual users of custom roles.
+* Added a 'Notification Type' column to the notifications screen.
+* Renamed 'User Roles' column in notifications screen to 'User Roles/Users'.
+
+= 1.0 =
+* First major release - we're no longer in beta!
+* Total overhaul. The plugin has been completely re-written.
+* Auto-importer: Your old notifications will be imported when updating the plugin.
+* New 'Generator' for easily creating and editing your notifications.
+* More options and notification types as per feedback via the forums - thanks everyone!
+* Granular control over choosing either user roles or individual users you'd like to send a notification to.
+* Fully translatable.
+* Loads of bugfixes and improvements.
+
+= 0.2.1 beta =
+* Minor Bugfixes
+
+= 0.2 beta =
+* Added an option to suppress spam comment notifications.
+
+= 0.1 beta =
+* Initial version of the plugin.
+* Settings page for configuring notification types for roles.
+* Email Templates page for customising emails using HTML and shortcodes.
+
+
+
+== Upgrade Notice ==
+
+= Migrating from 0.2.1 to 1.0 =
+
+Version 1 features an auto-importer which should migrate all of your notifications from any previous versions of the plugin to 1.0. 
+Please ensure that you check, double check and test your notifications before and after upgrading.
---
+++ includes/engine/class-bnfw-engine.php
@@ -0,0 +1,388 @@
+<?php
+/**
+ * BNFW Engine
+ *
+ * @since 1.0
+ */
+
+class BNFW_Engine {
+
+	/**
+	 * Send the notification email.
+	 *
+	 * @since 1.0
+	 * @param unknown $setting
+	 * @param unknown $id
+	 */
+	public function send_notification( $setting, $id ) {
+		$subject = $this->handle_shortcodes( $setting['subject'], $setting['notification'], $id );
+		$message = $this->handle_shortcodes( $setting['message'], $setting['notification'], $id );
+		$emails  = $this->get_emails( $setting );
+		$headers = $this->get_headers( $emails );
+		$headers[] = 'Content-type: text/html';
+
+		foreach ( $emails['to'] as $email ) {
+			wp_mail( $email, $subject, $message, $headers );
+		}
+	}
+
+	/**
+	 * Send new user registration notification email.
+	 *
+	 * @since 1.1
+	 * @param array  $setting  Notification setting
+	 * @param object $user     User object
+	 * @param string $password Plain text password
+	 */
+	public function send_registration_email( $setting, $user, $password = '' ) {
+		$user_id = $user->ID;
+
+		$subject = $this->handle_shortcodes( $setting['subject'], $setting['notification'], $user_id );
+		$message = $this->handle_shortcodes( $setting['message'], $setting['notification'], $user_id );
+
+		$subject = str_replace( '[password]', $password, $subject );
+		$message = str_replace( '[password]', $password, $message );
+
+		$subject = str_replace( '[login_url]', wp_login_url() , $subject );
+		$message = str_replace( '[login_url]', wp_login_url(), $message );
+
+		$headers = array( 'Content-type: text/html' );
+
+		wp_mail( $user->user_email, $subject, $message, $headers );
+	}
+
+	/**
+	 * Handle shortcode for password reset email message.
+	 *
+	 * @since 1.1
+	 */
+	public function handle_password_reset_shortcodes( $setting, $key, $user_login, $user_data ) {
+		$message = $this->user_shortcodes( $setting['message'], $user_data->ID );
+
+		$reset_link = wp_login_url() . "?action=rp&key=$key&login=$user_login";
+		$message = str_replace( '[password_reset_link]', $reset_link, $message );
+
+		return $message;
+	}
+
+	/**
+	 * Generate message for notification.
+	 *
+	 * @since 1.0
+	 * @param unknown $message
+	 * @param unknown $notification
+	 * @param unknown $id
+	 * @return unknown
+	 */
+	private function handle_shortcodes( $message, $notification, $id ) {
+		switch ( $notification ) {
+			case 'new-comment':
+			case 'new-trackback':
+			case 'new-pingback':
+				// handle new comments, trackbacks and pingbacks
+				$message = $this->comment_shortcodes( $message, $id );
+				$comment = get_comment( $id );
+				$message = $this->post_shortcodes( $message, $comment->comment_post_ID );
+				break;
+
+			case 'admin-password':
+			case 'user-password':
+			case 'admin-user':
+			case 'welcome-email':
+				// handle users (lost password and new user registration)
+				$message = $this->user_shortcodes( $message, $id );
+				break;
+
+			case 'new-category':
+				// handle new category
+				$message = $this->taxonomy_shortcodes( $message, 'category', $id );
+				break;
+
+			case 'new-post_tag':
+				// handle new tag
+				$message = $this->taxonomy_shortcodes( $message, 'post_tag', $id );
+				break;
+
+			default:
+				$type = explode( '-', $notification, 2 );
+				if ( 'newterm' == $type[0] ) {
+					// handle new terms
+					$message = $this->taxonomy_shortcodes( $message, $type[1], $id );
+
+				} else if ( 'new' == $type[0] || 'update' == $type[0] || 'pending' == $type[0] || 'future' == $type[0] ) {
+					// handle new, update and pending posts
+					$post_types = get_post_types( array( 'public' => true ), 'names' );
+					$post_types = array_diff( $post_types, array( BNFW_Notification::POST_TYPE ) );
+
+					if ( in_array( $type[1], $post_types ) ) {
+						$message = $this->post_shortcodes( $message, $id );
+						$post = get_post( $id );
+						$message = $this->user_shortcodes( $message, $post->post_author );
+					}
+				} else if ( 'comment' == $type[0] ) {
+					$message = $this->comment_shortcodes( $message, $id );
+					$comment = get_comment( $id );
+					$message = $this->post_shortcodes( $message, $comment->comment_post_ID );
+				}
+				break;
+		}
+
+		return $message;
+	}
+
+	/**
+	 * Handle post shortcodes.
+	 *
+	 * @since 1.0
+	 * @param unknown $message
+	 * @param unknown $post_id
+	 * @return unknown
+	 */
+	private function post_shortcodes(  $message, $post_id  ) {
+		$post = get_post(  $post_id  );
+
+		$post_content = apply_filters( 'the_content', $post->post_content );
+		$post_content = str_replace( ']]>', ']]&gt;', $post_content );
+
+		$message = str_replace( '[ID]', $post->ID, $message );
+		$message = str_replace( '[post_author]', $post->post_author, $message );
+		$message = str_replace( '[post_date]', $post->post_date, $message );
+		$message = str_replace( '[post_date_gmt]', $post->post_date_gmt, $message );
+		$message = str_replace( '[post_content]', $post_content, $message );
+		$message = str_replace( '[post_title]', $post->post_title, $message );
+		$message = str_replace( '[post_excerpt]', $post->post_excerpt, $message );
+		$message = str_replace( '[post_status]', $post->post_status, $message );
+		$message = str_replace( '[comment_status]', $post->comment_status, $message );
+		$message = str_replace( '[ping_status]', $post->ping_status, $message );
+		$message = str_replace( '[post_password]', $post->post_password, $message );
+		$message = str_replace( '[post_name]', $post->post_name, $message );
+		$message = str_replace( '[to_ping]', $post->to_ping, $message );
+		$message = str_replace( '[pinged]', $post->pinged, $message );
+		$message = str_replace( '[post_modified]', $post->post_modified, $message );
+		$message = str_replace( '[post_modified_gmt]', $post->post_modified_gmt, $message );
+		$message = str_replace( '[post_content_filtered]', $post->post_content_filtered, $message );
+		$message = str_replace( '[post_parent]', $post->post_parent, $message );
+		$message = str_replace( '[guid]', $post->guid, $message );
+		$message = str_replace( '[menu_order]', $post->menu_order, $message );
+		$message = str_replace( '[post_type]', $post->post_type, $message );
+		$message = str_replace( '[post_mime_type]', $post->post_mime_type, $message );
+		$message = str_replace( '[comment_count]', $post->comment_count, $message );
+		$message = str_replace( '[permalink]', get_permalink( $post->ID ), $message );
+		if ( 'future' == $post->post_status ) {
+			$message = str_replace( '[post_scheduled_date]', $post->post_date, $message );
+			$message = str_replace( '[post_scheduled_date_gmt]', $post->post_date_gmt, $message );
+		} else {
+			$message = str_replace( '[post_scheduled_date]', 'Published', $message );
+			$message = str_replace( '[post_scheduled_date_gmt]', 'Published', $message );
+		}
+
+		$category_list = implode( ',', wp_get_post_categories( $post_id, array( 'fields' => 'names' ) ) );
+		$message = str_replace( '[post_category]', $category_list, $message );
+
+		$tag_list = implode( ',', wp_get_post_tags( $post_id, array( 'fields' => 'names' ) ) );
+		$message = str_replace( '[post_tag]', $tag_list, $message );
+
+		return $message;
+	}
+
+	/**
+	 * Handle comment shortcodes.
+	 *
+	 * @since 1.0
+	 * @param unknown $message
+	 * @param unknown $comment_id
+	 * @return unknown
+	 */
+	private function comment_shortcodes( $message, $comment_id ) {
+		$comment = get_comment( $comment_id );
+
+		$message = str_replace( '[comment_ID]', $comment->comment_ID, $message );
+		$message = str_replace( '[comment_post_ID]', $comment->comment_post_ID, $message );
+		$message = str_replace( '[comment_author]', $comment->comment_author, $message );
+		$message = str_replace( '[comment_author_email]', $comment->comment_author_email, $message );
+		$message = str_replace( '[comment_author_url]', $comment->comment_author_url, $message );
+		$message = str_replace( '[comment_author_IP]', $comment->comment_author_IP, $message );
+		$message = str_replace( '[comment_date]', $comment->comment_date, $message );
+		$message = str_replace( '[comment_date_gmt]', $comment->comment_date_gmt, $message );
+		$message = str_replace( '[comment_content]', get_comment_text( $comment->comment_ID ), $message );
+		$message = str_replace( '[comment_karma]', $comment->comment_karma, $message );
+		$message = str_replace( '[comment_approved]', str_replace( array( '0', '1', 'spam' ), array( 'awaiting moderation', 'approved', 'spam' ), $comment->comment_approved ), $message );
+		$message = str_replace( '[comment_agent]', $comment->comment_agent, $message );
+		$message = str_replace( '[comment_type]', $comment->comment_type, $message );
+		$message = str_replace( '[comment_parent]', $comment->comment_parent, $message );
+		$message = str_replace( '[user_id]', $comment->user_id, $message );
+		$message = str_replace( '[permalink]', get_comment_link( $comment->comment_ID ), $message );
+
+		return $message;
+	}
+
+	/**
+	 * Handle user shortcodes.
+	 *
+	 * @access private
+	 * @since 1.0
+	 * @param unknown $message
+	 * @param unknown $user_id
+	 * @return unknown
+	 */
+	private function user_shortcodes( $message, $user_id ) {
+		$user_info = get_userdata( $user_id );
+
+		$message = str_replace( '[ID]', $user_info->ID, $message );
+		$message = str_replace( '[user_login]', $user_info->user_login, $message );
+		$message = str_replace( '[user_nicename]', $user_info->user_nicename, $message );
+		$message = str_replace( '[user_email]', $user_info->user_email, $message );
+		$message = str_replace( '[user_url]', $user_info->user_url, $message );
+		$message = str_replace( '[user_registered]', $user_info->user_registered, $message );
+		$message = str_replace( '[display_name]', $user_info->display_name, $message );
+		$message = str_replace( '[user_firstname]', $user_info->user_firstname, $message );
+		$message = str_replace( '[user_lastname]', $user_info->user_lastname, $message );
+		$message = str_replace( '[nickname]', $user_info->nickname, $message );
+		$message = str_replace( '[user_description]', $user_info->user_description, $message );
+		if ( is_array( $user_info->wp_capabilities ) ) {
+			$message = str_replace( '[wp_capabilities]', implode( ',', $user_info->wp_capabilities ), $message );
+		}
+		$message = str_replace( '[admin_color]', $user_info->admin_color, $message );
+		$message = str_replace( '[closedpostboxes_page]', $user_info->closedpostboxes_page, $message );
+		$message = str_replace( '[rich_editing]', $user_info->rich_editing, $message );
+
+		return $message;
+	}
+
+	/**
+	 * Handle taxonomy shortcodes.
+	 *
+	 * @access private
+	 * @since 1.1
+	 * @param string $message
+	 * @param string $taxonomy
+	 * @param int $term_id
+	 * @return string
+	 */
+	private function taxonomy_shortcodes( $message, $taxonomy, $term_id ) {
+		$term_info = get_term( $term_id, $taxonomy );
+
+		$message = str_replace( '[slug]', $term_info->slug, $message );
+		$message = str_replace( '[name]', $term_info->name, $message );
+		$message = str_replace( '[description]', $term_info->description, $message );
+
+		return $message;
+	}
+
+	/**
+	 * Get the list of emails from the notification settings.
+	 *
+	 * @since 1.0
+	 * @param unknown $setting
+	 * @return unknown
+	 */
+	private function get_emails( $setting ) {
+		$emails = array();
+		if ( ! empty( $setting['from-name'] ) && ! empty( $setting['from-email'] ) ) {
+			$emails['from'] = $setting['from-name'] . ' <' . $setting['from-email'] . '>' ;
+		} else {
+			$emails['from'] = get_option( 'blogname' ) . ' <' . get_option( 'admin_email' ) . '>' ;
+		}
+
+		if ( ! empty( $setting['users'] ) ) {
+			$emails['to'] = $this->get_emails_from_id( $setting['users'] );
+		} else {
+			$emails['to'] = $this->get_emails_from_role( $setting['user-roles'] );
+		}
+
+		$emails['cc'] = $this->get_emails_from_role( $setting['cc-roles'] );
+		if ( ! empty( $setting['cc-email'] ) ) {
+			$emails['cc'][] = $setting['cc-email'];
+		}
+
+		$emails['bcc'] = $this->get_emails_from_role( $setting['bcc-roles'] );
+		if ( ! empty( $setting['bcc-email'] ) ) {
+			$emails['bcc'][] = $setting['bcc-email'];
+		}
+
+		return $emails;
+	}
+
+	/**
+	 * Get user emails by user ids.
+	 *
+	 * @since 1.0
+	 * @param mixed   $user_ids
+	 * @return unknown
+	 */
+	private function get_emails_from_id( $user_ids ) {
+		$email_list = array();
+		$user_query = new WP_User_Query( array( 'include' => $user_ids ) );
+		foreach ( $user_query->results as $user ) {
+			$email_list[] = $user->user_email;
+		}
+		return $email_list;
+	}
+
+	/**
+	 * Get emails of users based on role.
+	 *
+	 * @since 1.0
+	 * @param unknown $roles
+	 * @return unknown
+	 */
+	private function get_emails_from_role( $roles ) {
+		if ( ! is_array( $roles ) ) {
+			$roles = array( $roles );
+		}
+
+		$email_list = array();
+		foreach ( $roles as $role ) {
+			$role_name = $this->get_role_name_by_label( $role );
+			$users = get_users( array(
+					'role' => $role_name,
+					'fields' => array( 'user_email' ),
+				) );
+
+			foreach ( $users as $user ) {
+				$email_list[] = $user->user_email;
+			}
+		}
+
+		return $email_list;
+	}
+
+	/**
+	 * Get User role name by label.
+	 *
+	 * @param mixed   $role_label
+	 * @return unknown
+	 */
+	protected function get_role_name_by_label( $role_label ) {
+		global $wp_roles;
+		foreach ( $wp_roles->roles as $role_name => $role_info ) {
+			if ( $role_label == $role_info['name'] ) {
+				return $role_name;
+			}
+		}
+
+		// There is something wrong
+		return '';
+	}
+
+	/**
+	 * Generate email headers based on the emails.
+	 *
+	 * @since 1.0
+	 * @param unknown $emails
+	 * @return unknown
+	 */
+	private function get_headers( $emails ) {
+		$headers = array();
+		return $headers;
+
+		$headers[] = 'From:' . $emails['from'];
+		if ( ! empty( $emails['cc'] ) ) {
+			$headers[] = 'Cc:' . implode( ',', $emails['cc'] );
+		}
+		if ( ! empty( $emails['bcc'] ) ) {
+			$headers[] = 'Bcc:' . implode( ',', $emails['bcc'] );
+		}
+	}
+}
+?>
